c32c97bc6ea1415cf0a68ea8933b717e91d697e5
NB2P <leo.p.longo@hotmail.com>
2022-05-25 21:22:19 -0300

    Ad alerta ao submeter o forms com sucesso

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-const btnEnviar = document.getElementById("send")
+const btnEnviar = document.getElementById("send");
 
-btnEnviar.addEventListener("click", validarCampos)
+btnEnviar.addEventListener("click", validarCampos);
 
-function validarCampos(){
-    const campos = document.querySelectorAll("input[type='text'], input[type='email'], input[type='date'], input[type='display']")
+function validarCampos() {
+	const campos = document.querySelectorAll("input[type='text'], input[type='email'], input[type='date'], input[type='display']");
+	for (let x = 0; x < campos.length; x++) {
+		if (campos[x].value == "") {
+			alert("Atenção, o campo " + campos[x].name + " não está preenchido!");
+			return;
+		}
+	}
+}
 
-    for(let x = 0; x < campos.length; x++){
-        if (campos[x].value == ''){
-            alert("Atenção, o campo " + campos[x].name + " não está preenchido!")
-            return
-        }else{
-            alert("Formulário enviado com sucesso!")
-        }
-    }
+function alertaSubmetido() {
+	alert("Formulário enviado com sucesso!");
 }
